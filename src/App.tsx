8fbe5eeaa4424ac742daa7d784442aaeb721d75b
Nicholas Eduardo <nicholasess@gmail.com>
2019-02-18 21:44:32 -0300

    remove component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import Header from "./components/header"
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        <Header />
+        
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
